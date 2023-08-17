5ce110cbdf054bfd608ba438a796a715df74e972
hlky <hlky@hlky.ac>
2023-08-17 19:27:48 +0100

    Revert "no longer required with 8px increment modules"
    This reverts commit cca0383915f7fffa0bddca1532949aa63aedfe1c.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .AITemplate.AITemplate import AITemplateLoader, AITemplateControlNetLoader, AITemplateVAEDecode, AITemplateVAEEncode, VAEEncodeForInpaint
+from .AITemplate.AITemplate import AITemplateLoader, AITemplateControlNetLoader, AITemplateVAEDecode, AITemplateVAEEncode, VAEEncodeForInpaint, AITemplateEmptyLatentImage, AITemplateLatentUpscale
 
 NODE_CLASS_MAPPINGS = {
     "AITemplateLoader": AITemplateLoader,
@@ -6,6 +6,8 @@ NODE_CLASS_MAPPINGS = {
     "AITemplateVAEDecode": AITemplateVAEDecode,
     "AITemplateVAEEncode": AITemplateVAEEncode,
     "AITemplateVAEEncodeForInpaint": VAEEncodeForInpaint,
+    "AITemplateEmptyLatentImage": AITemplateEmptyLatentImage,
+    "AITemplateLatentUpscale": AITemplateLatentUpscale,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
@@ -16,4 +18,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "AITemplateVAEDecode": "VAE Decode (AITemplate)",
     "AITemplateVAEEncode": "VAE Encode (AITemplate)",
     "AITemplateVAEEncodeForInpaint": "VAE Encode (AITemplate, Inpaint)",
+    "AITemplateEmptyLatentImage": "Empty Latent Image (AITemplate)",
+    "AITemplateLatentUpscale": "Upscale Latent Image (AITemplate)",
 }
